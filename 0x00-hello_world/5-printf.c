428518f0e234fb86db5c96e541c892c23d87d62a
mehdi <bouayabenmehdi@gmail.com>
2023-07-13 10:04:36 -0700

    commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints a string to the standard output
+ *
+ * Return: 0 if successful, non-zero otherwise
+ */
+int main(void)
+{
+    printf("with proper grammar, but the outcome is a piece of art,\n");
+    return (0);
+}
