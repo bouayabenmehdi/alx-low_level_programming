b30dc06daa5b77a61ae0de3b053e9aecffd6ab7c
mehdi <bouayabenmehdi@gmail.com>
2023-07-13 13:45:39 -0700

    commit2

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - prints exactly "Programming is like building a multilingual puzzle"
+ *main - Entry point
  *
- * Return: 0
+ *Return: Always 0 (Success)
  */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle\"");
-
-    return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
-
